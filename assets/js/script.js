46152a692c12999322f6b41706b1af2e1e42f26b
Hannahdlewis <hannahdlewis07@gmail.com>
2021-07-16 09:08:06 -0400

    added questions for quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 console.log("Your external JavaScript file is linked 🎉");
 
+// Questions for quiz
+
 var myQuestions = [
 	{
 		question: "What is a county in GA?",
@@ -30,3 +32,40 @@ var myQuestions = [
 	}
 
 ];
+
+// 
+
+function showQuestions(questions, quizContainer){
+	// we'll need a place to store the output and the answer choices
+	var output = [];
+	var answers;
+
+	// for each question...
+	for(var i=0; i<questions.length; i++){
+		
+		// first reset the list of answers
+		answers = [];
+
+		// for each available answer to this question...
+		for(letter in questions[i].answers){
+
+			// ...add an html radio button
+			answers.push(
+				'<label>'
+					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+					+ letter + ': '
+					+ questions[i].answers[letter]
+				+ '</label>'
+			);
+		}
+
+		// add this question and its answers to the output
+		output.push(
+			'<div class="question">' + questions[i].question + '</div>'
+			+ '<div class="answers">' + answers.join('') + '</div>'
+		);
+	}
+
+	// finally combine our output list into one string of html and put it on the page
+	quizContainer.innerHTML = output.join('');
+}
