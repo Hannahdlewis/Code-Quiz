cc8afa4d20003050fa0c6c6a2c70ceac3d58be0e
Hannahdlewis <hannahdlewis07@gmail.com>
2021-07-16 09:02:59 -0400

    updated js with questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
 console.log("Your external JavaScript file is linked 🎉");
+
+var myQuestions = [
+	{
+		question: "What is a county in GA?",
+		answers: {
+			a: 'Austin',
+			b: 'Fulton',
+			c: 'Los Angeles'
+		},
+		correctAnswer: 'b'
+	},
+	{
+		question: " Blue and yellow makes what color?",
+		answers: {
+			a: 'Pink',
+			b: 'Orange',
+			c: 'Green'
+		},
+		correctAnswer: 'c'
+	}
+];
