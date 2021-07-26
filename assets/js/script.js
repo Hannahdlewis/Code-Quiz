e9f8e6908659b1b5241e710eafeed2f51badcf30
Hannahdlewis <hannahdlewis07@gmail.com>
2021-07-25 20:54:58 -0400

    updated var button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 var startButton = document.getElementById('start-btn')
-var scoreButton = document.getElementById('start-btn')
-var startbutton = document.getElementById('start-btn')
-var startbutton = document.getElementById('start-btn')
-var startbutton = document.getElementById('start-btn')
-var startbutton = document.getElementById('start-btn')
+var scoreButton = document.getElementById('high-scores')
+var startbutton = document.getElementById('question-box')
+var startbutton = document.getElementById('question')
+var startbutton = document.getElementById('answer-choice')
+var startbutton = document.getElementById('time-left')
 
 var isWin = false;
 var timer;
