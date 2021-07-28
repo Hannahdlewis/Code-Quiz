2428fda746532f8c53ae73f8902e63b0b932ac67
Hannahdlewis <hannahdlewis07@gmail.com>
2021-07-27 21:04:37 -0400

    udpated js high score

---
+++ assets/js/highscore.js
@@ -0,0 +1,13 @@
+var backBtn = document.querySelector('.go-back');
+var clearBtn = document.querySelector('.clear');
+var score_list = document.querySelector('#highscores');
+
+var list = [];
+
+backBtn.addEventListener('click', function() {
+
+    location.href = 'https://hannahdlewis.github.io/Code-Quiz/'
+});
+
+
+
