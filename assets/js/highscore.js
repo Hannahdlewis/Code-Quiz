3236bdc9e99752ce73dec99d51d0a14ceec18814
Hannahdlewis <hannahdlewis07@gmail.com>
2021-07-27 21:07:35 -0400

    udpated js high score

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -9,5 +9,12 @@ backBtn.addEventListener('click', function() {
     location.href = 'https://hannahdlewis.github.io/Code-Quiz/'
 });
 
+function init(){
+    var user_info =JSON.parse(localStorage.getItem('user-cred'));
+// not sure if this works, double check may need to change to my stuff
+    list = user_info;
+    console.log(user_info);
+    renderList();
+}
 
 
